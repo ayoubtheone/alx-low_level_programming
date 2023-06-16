bcccc2f220c3c23f90340dc38e83472ed303e86e
ayoubbo <ayoubbouaziz.33@gmail.com>
2023-06-16 16:00:52 -0700

    10.2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Succes)
- */
+int main() {
+    int i, j;
 
-int main(void)
-{
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+    for (i = 0; i <= 99; i++) {
+        for (j = i; j <= 99; j++) {
+            int first_digit_i = i / 10;
+            int second_digit_i = i % 10;
+            int first_digit_j = j / 10;
+            int second_digit_j = j % 10;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
+            putchar('0' + first_digit_i);
+            putchar('0' + second_digit_i);
+            putchar(' ');
+            putchar('0' + first_digit_j);
+            putchar('0' + second_digit_j);
+            putchar(',');
 
-	for (hun = 0 ; hun <= 10 ; hun++)
-	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
-		}
-	}
-}
-	putchar('\n');
-	return (0);
+            if (i != j) {
+                putchar(' ');
+                putchar('0' + first_digit_j);
+                putchar('0' + second_digit_j);
+                putchar(' ');
+                putchar('0' + first_digit_i);
+                putchar('0' + second_digit_i);
+                putchar(',');
+            }
+        }
+    }
+
+    return 0;
 }
