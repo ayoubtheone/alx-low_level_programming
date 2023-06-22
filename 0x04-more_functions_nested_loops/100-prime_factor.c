0e96dfe0bda6937c85aff9740bec75527378a667
ayoubbo <ayoubbouaziz.33@gmail.com>
2023-06-22 16:45:41 -0700

    4 to s0

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,46 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - entry point
- *
- * Return: always 0 (sucess)
+ * print_number -  checks for checks for a digit (0 through 9).
+ * @n: n -  Variable
+ * Return: Always 0.
  */
-int main(void)
+void print_number(int n)
 {
-	unsigned long int num = 612852475143;
-	unsigned long int prim;
+	unsigned int z;
+	int m, b;
 
-	prim = 3;
-	while (prim < num / 2)
+	b = 10;
+
+	if (n < 10 && n >= 0)
 	{
-		if ((num % prim) == 0)
-		{
-			if((prim % 3) == 2)
-				printf(",%lu ", prim);
-		}
-
-		prim+=2;
+		_putchar (n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n - 2 * n;
+		_putchar('-');
+		_putchar (n + '0');
 	}
 
-	putchar('\n');
-	return (0);
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar ('-');
+		}
+		z = n;
+	while (z / b > 9)
+        {
+		b = b * 10;
+	}
+	while (b > 0)
+	{
+		m = z / b;
+		z = z % b;
+		_putchar (m + '0');
+		b = b / 10;
+	}
+	}
 }
